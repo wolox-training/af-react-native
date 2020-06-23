233f8df2377a108d3aaaa89a50e737099efbb32e
ArielFerraris <ariel.ferraris@wolox.com.ar>
2020-06-23 10:59:49 -0300

    redux

--- src/app/screens/library/index.tsx
+++ src/app/screens/library/index.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, FlatList } from 'react-native';
+import { connect } from 'react-redux';
 
 import BookCard from './components/bookCard'
 import { booksList } from '@constants/mocks';
 import { Book } from '@interfaces/books';
 
 import styles from './styles';
+import { actionCreator } from '../../../redux/books/actions';
 
 const renderItem =({item}: Book) => (
   <BookCard item={item} />
 );
 
-const Library = () => {
+const Library = (props) => {
+  // const [booksList, setBookList] = useState([]);
+  
+  useEffect(() => {
+    // const books = getBooks();
+    console.log(props.getBooks())
+  },[])
+
   return(
     <View style={styles.container}>
-      <FlatList 
-        data={booksList}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
-      />
+      {booksList && 
+        <FlatList 
+          data={booksList}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
+      }
     </View>
   )
 }
 
-export default Library;
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getBooks: () => dispatch(actionCreator.getBooks())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Library);
