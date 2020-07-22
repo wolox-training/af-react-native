45ac214ca40bfceab0a1d0a8b6ca4ee08604f6ab
ArielFerraris <ariel.ferraris@wolox.com.ar>
2020-07-22 12:36:42 -0300

    authsetup in App

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useEffect } from 'react';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { Provider } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { actionCreator } from '@redux/auth/actions';
+
 
 import store from './src/redux/store';
 import AppNavigation from './AppNavigation';
 
 const App = () => {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(actionCreator.authSetup(navigation))
+  }, [dispatch])
+
   return (
     <Provider store={store}>
       <NavigationContainer>
